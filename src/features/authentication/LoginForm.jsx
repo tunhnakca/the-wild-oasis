b5e258961a8ef78332ae32d58325b56681719b75
Tunahan Akça <tunahanakca@Tunahan-Mac-mini.local>
2026-02-21 23:23:40 +0300

    Last files uploaded and updated

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -8,7 +8,7 @@ import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
   const [email, setEmail] = useState("tunahan@example.com");
-  const [password, setPassword] = useState("1767");
+  const [password, setPassword] = useState("12345678");
   const { login, isPending } = useLogin();
 
   function handleSubmit(e) {
